6c1fe7048c17ab33a13e78479470f92d2f57a61d
katerynahania <ekaterinademkovskaia@gmail.com>
2022-08-15 14:23:54 +0200

    day38: Inheritance - Method Overreading. Car Task.

--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -1,4 +1,13 @@
 package day38_Inheritance.carTask;
 
-public class Toyota {
+public class Toyota extends Car {
+
+    public Toyota(String brand, String model, int year, double price, String colour, int miles) {
+        super(brand, model, year, price, colour, miles);
+    }
+
+    public void reliabile(){
+        System.out.println(brand+" "+model+" is reliable");
+    }
+
 }
