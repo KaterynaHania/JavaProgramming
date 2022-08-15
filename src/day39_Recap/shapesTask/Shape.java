ffb34f249e31bfa2cfbb0a7ad73a6b659a6610fe
katerynahania <ekaterinademkovskaia@gmail.com>
2022-08-15 19:09:35 +0200

    day39: Recap.

--- src/day39_Recap/shapesTask/Shape.java
+++ src/day39_Recap/shapesTask/Shape.java
@@ -31,7 +31,16 @@ public class Shape {
       return 0;
    }
 
-   /*
+   @Override
+   public String toString() {
+      return "Shape{" +
+              "name='" + name + '\'' +
+              ", area='" + area() + '\'' +
+              ", perimeter='" + perimeter() + '\'' +
+              '}';
+   }
+
+  /*
    Shape:
 	variable:
 		name
