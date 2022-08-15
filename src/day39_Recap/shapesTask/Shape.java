5d90afb5b76535e33739e3f05c87e8ee3901680b
katerynahania <ekaterinademkovskaia@gmail.com>
2022-08-15 18:55:45 +0200

    day39: Recap.

--- src/day39_Recap/shapesTask/Shape.java
+++ src/day39_Recap/shapesTask/Shape.java
@@ -3,13 +3,35 @@ package day39_Recap.shapesTask;
 public class Shape {
    private String name;
 
-   public Shape(String name) {
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      if (name==null){
+         System.err.println("Name can not be null");
+         System.exit(1);//1 smth went wrong
+      }
+      if (name.isEmpty()||name.isBlank()){
+         System.err.println("Invalid name");
+         System.exit(1);
+      }
       this.name = name;
    }
 
+   public Shape(String name) {
+      setName(name);
+   }
 
+   public double area(){
+      return 0;
+   }
 
-/*
+   public double perimeter(){
+      return 0;
+   }
+
+   /*
    Shape:
 	variable:
 		name
