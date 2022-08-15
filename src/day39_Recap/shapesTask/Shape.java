1f24c363d795ada6d4d583977d4e1ea65c48acf6
katerynahania <ekaterinademkovskaia@gmail.com>
2022-08-15 18:48:08 +0200

    day39: Recap.

---
+++ src/day39_Recap/shapesTask/Shape.java
@@ -0,0 +1,25 @@
+package day39_Recap.shapesTask;
+
+public class Shape {
+   private String name;
+
+   public Shape(String name) {
+      this.name = name;
+   }
+
+
+
+/*
+   Shape:
+	variable:
+		name
+
+	Add A constructor to set the filed
+
+	Methods:
+		area(){return 0}
+		perimeter{return 0}
+		toString(): name, area, perimeter
+
+    */
+}
