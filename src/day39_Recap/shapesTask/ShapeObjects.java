74f3aa3611e5cf5528f17c86007e433b0941fc8c
katerynahania <ekaterinademkovskaia@gmail.com>
2022-08-16 16:08:51 +0200

    day39: Recap.

--- src/day39_Recap/shapesTask/ShapeObjects.java
+++ src/day39_Recap/shapesTask/ShapeObjects.java
@@ -6,16 +6,19 @@ public class ShapeObjects {
 
       Square square = new Square(10);
         System.out.println(square);
+        System.out.println(square.getName());
 
         Rectangle rectangle = new Rectangle(5,6);
         System.out.println(rectangle);
         rectangle.setWidth(5);
         System.out.println(rectangle);
+        System.out.println(rectangle.getName());
 
         Circle circle = new Circle(10);
         System.out.println(circle);
         circle.setRadius(5);
         System.out.println(circle);
+        System.out.println(circle.getName());
 
 
     }
