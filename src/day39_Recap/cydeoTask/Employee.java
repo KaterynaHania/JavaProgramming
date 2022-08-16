99a3f69aac89cd4efeae7f5b6c8625acfbcdef15
katerynahania <ekaterinademkovskaia@gmail.com>
2022-08-16 17:39:38 +0200

    day39: Recap.

---
+++ src/day39_Recap/cydeoTask/Employee.java
@@ -0,0 +1,78 @@
+package day39_Recap.cydeoTask;
+
+public class Employee extends Person{
+
+    private int employeeId;
+    private String jobTitle;
+    private double salary;
+
+    public Employee(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender);
+        setEmployeeId(employeeId);
+        setJobTitle(jobTitle);
+        setSalary(salary);
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        if (employeeId<=0){
+            System.err.println("Invalid ID: "+employeeId);
+            System.exit(1);
+        }
+        this.employeeId = employeeId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        if (jobTitle.isEmpty()||jobTitle.isBlank()){
+            System.err.println("Invalid job title");
+            System.exit(1);
+        }
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary<=0){
+            System.err.println("Invalid salary");
+            System.exit(1);
+        }
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + getName() + '\'' +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", employeeId=" + employeeId +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+   /*
+     2. Create a sub class of Person named Employee:
+            Extra variables:
+                employeeId, jobTitle, salary
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Methods:
+                work()
+                toString()
+
+     */
+}
